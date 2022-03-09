eb24a33624642d1e36ffe2f2c260b5808c9108af
HongJinHyeok <redmax45@naver.com>
2022-03-09 23:10:15 +0900

    userModule 추가

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from 'src/user/user.module';
 import { BoardRepository } from './board.repository';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BoardRepository])],
+  imports: [TypeOrmModule.forFeature([BoardRepository]), UserModule],
   controllers: [BoardsController],
   providers: [BoardsService],
 })

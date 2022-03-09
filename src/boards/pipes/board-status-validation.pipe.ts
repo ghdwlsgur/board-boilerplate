edbbd6dadbb4d330f545c712670ce7edcf1b7746
HongJinHyeok <redmax45@naver.com>
2022-03-09 23:11:00 +0900

    유효성체크 파이프라인 생성

---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,17 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { BoardStatus } from '../board-status.enum';
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions = [BoardStatus.PUBLIC, BoardStatus.PRIVATE];
+  transform(value: any) {
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value} isn't in the status options`);
+    }
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    const index = this.StatusOptions.indexOf(status);
+    return index !== -1;
+  }
+}

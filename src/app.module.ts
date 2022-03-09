fae40344dc56ae3e473db381f3b4edd556f92ab4
HongJinHyeok <redmax45@naver.com>
2022-03-09 23:06:46 +0900

    UserModule 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { MySqlConfigModule } from 'src/config/database/config.module';
 import { MySqlConfigService } from 'src/config/database/config.service';
 import { BoardsModule } from './boards/boards.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { BoardsModule } from './boards/boards.module';
       inject: [MySqlConfigService],
     }),
     BoardsModule,
+    UserModule,
   ],
 })
 export class AppModule {}

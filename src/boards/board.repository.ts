582246f50a8e0b441243b8ced7d5a5eefaefcd37
HongJinHyeok <redmax45@naver.com>
2022-03-09 23:07:23 +0900

    user 추가

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -1,3 +1,4 @@
+import { User } from 'src/user/user.entity';
 import { EntityRepository, Repository } from 'typeorm';
 import { BoardStatus } from './board-status.enum';
 import { Board } from './board.entity';
@@ -5,12 +6,16 @@ import { CreateBoardDto } from './dto/create-board.dto';
 
 @EntityRepository(Board)
 export class BoardRepository extends Repository<Board> {
-  async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+  async createBoard(
+    createBoardDto: CreateBoardDto,
+    user: User,
+  ): Promise<Board> {
     const { title, description } = createBoardDto;
     const board = this.create({
       title,
       description,
       status: BoardStatus.PUBLIC,
+      user,
     });
     await this.save(board);
     return board;
